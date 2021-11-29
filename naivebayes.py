bee19cc5a56a8d06871eb7e97ca40c4c0a7cfb1c
Durante-C <89756294+Durante-C@users.noreply.github.com>
2021-11-29 03:32:59 -0500

    Add files via upload

--- naivebayes.py
+++ naivebayes.py
@@ -1,10 +1,7 @@
-# From Kmeans HW
 import scipy.io as sc
 from scipy import signal, linalg
 import numpy as np
 import pandas as pd
-from collections import Counter
-import matplotlib.pyplot as plt
 
 #From Nico: for import
 import csv
@@ -13,7 +10,7 @@ import csv
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-from sklearn.cluster import KMeans
+from sklearn.naive_bayes import MultinomialNB
 
 #IMPORTING DATA FROM MASTER CSV -- Note: Already mirrors form of data matrix from HW7
 reader = csv.reader(open("data.csv", "r"), delimiter=",")
@@ -35,6 +32,7 @@ for row in X:
 
 #converting data and delineating into testing/training
 data = np.array(X).astype("float") 
+data = abs(data)
 print(data.shape)
 
 
@@ -42,22 +40,16 @@ print(data.shape)
 #y_train/test -- sets of 80/20% of correct genres
 x_train, x_test, y_train, y_test = train_test_split(data[:, 1 : -1], data[:, -1], test_size=0.2, random_state=0)
 
-#Generating, fitting, predicting, computing accuracy
-k_mean = KMeans(n_clusters=10, max_iter=10000)
-k_mean.fit(x_train)
-k_mean.set_params()
+#print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
 
-assigned_label = k_mean.fit_predict(x_train)
-predict_label = k_mean.fit_predict(x_test)
+nb = MultinomialNB()
+nb.fit(x_train, y_train)
+
+assigned_label = nb.predict(x_train)
+predict_label = nb.predict(x_test)
 
 train_acc = sum(assigned_label == y_train) / len(x_train)
 test_acc = sum(predict_label == y_test) / len(x_test)
 
 print("Training Accuracy: ", train_acc)
 print("Testing Accuracy: ", test_acc)
-
-#PLOTTING genre label frequency
-#plt.hist(assigned_label, bins=np.arange(assigned_label.min(), assigned_label.max()+1))
-#plt.show()
-#plt.xlabel("genre by number")
-#plt.ylabel("number of songs assigned to that genre")
