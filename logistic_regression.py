427521f62fb7821a8bb0fb99740de06dd215f03d
Nico Rossi <ncr3@rice.edu>
2021-12-02 01:40:50 -0600

    Updated logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,92 +3,23 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-reader = csv.reader(open("data.csv", "r"), delimiter=",")
-x = list(reader)
+from manage_csv import load_csv, write_submission
 
-# Remove header
-x = x[1 :]
-
-#FITTING
-
-# Remove song name and convert genre to a number
-genre_to_int = {}
-int_to_genre = {}
-genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
-val = 0
-for g in genres:
-    genre_to_int[g] = val
-    int_to_genre[val] = g
-    val += 1
-for row in x:
-    row.pop(0)
-    row[-1] = genre_to_int[row[-1]]
-
-data = np.array(x).astype("float")
+X, y, _ = load_csv("train_data.csv", True)
 
 # Random state 0 = 57%
 # Random state 100 = 58%
-X_train, X_test, y_train, y_test = train_test_split(data[:, 1 : -1], data[:, -1], test_size=0.2, random_state=100)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
 
-print(X_train.shape)
-print(X_test.shape)
-print(y_train.shape)
-print(y_test.shape)
-
-# # Only the mfcc vals and their class
-# x_data = data[ : , 1 :]
-
-# x_train_data = x_data[np.random.choice(x_data.shape[0], 450, replace=False), :]
-# x_train_vals = x_train_data[:, : -1]
-# x_train_class = x_train_data[:, -1]
-
-# print(x_train_vals.shape)
-# print(x_train_class.shape)
-
-log_reg = LogisticRegression(max_iter=1000000)
+log_reg = LogisticRegression(max_iter=1000000000)
 log_reg.fit(X_train, y_train)
 print(f'Train accuracy: {log_reg.score(X_train, y_train)}')
 print(f'Test accuracy: {log_reg.score(X_test, y_test)}')
 
-
-
 #TESTING
 
+X_test, _, original = load_csv("test_data.csv", False)
 
-reader = csv.reader(open("test_data.csv", "r"), delimiter=",")
-test_data = list(reader)
+labels = log_reg.predict(X_test)
 
-# Remove header
-test_data = test_data[1 :]
-
-x = []
-# Remove filename
-for row in test_data:
-    x.append(row[1:])
-
-data = np.array(x).astype("float")
-
-print(data.shape)
-
-labels = log_reg.predict(data)
-
-print(labels.shape)
-
-labels = [int_to_genre[i] for i in labels]
-
-print(labels[0:10])
-
-
-header = 'filename label'
-header = header.split()
-
-file = open('submission.csv', 'w', newline='')
-with file:
-    writer = csv.writer(file)
-    writer.writerow(header)
-
-for filename, label in zip(test_data, labels):
-    file = open('submission.csv', 'a', newline='')
-    with file:
-        writer = csv.writer(file)
-        writer.writerow((filename[0], label))
+write_submission("log_reg_submission.csv", labels, original)
